e1c75e4ca47317916d76ffc30d6a7146d6dfa2b7
Icaro Ramos <polosnramos@hotmail.com>
2023-05-23 21:29:19 -0300

    feat: theme and @types

---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+export default {
+  COLORS: {
+    RED_DARK: "#BF3B44",
+    RED_MID: "#F3BABD",
+    RED_LIGHT: "#F4E6E7",
+
+    GREEN_DARK: "#639339",
+    GREEN_MID: "#CBE4BA",
+    GREEN_LIGHT: "#E5F0DB",
+
+    GRAY_1: "#1B1D1E",
+    GRAY_2: "#333638",
+    GRAY_3: "#5C6265",
+    GRAY_4: "#B9BBBC",
+    GRAY_5: "#DDDEDF",
+    GRAY_6: "#EFF0F0",
+    GRAY_7: "#FAFAFA",
+
+    WHITE: "#FFFFFF",
+  },
+
+  FONT_WEIGHTS: {
+    NORMAL: "Nunito_Sans_400Regular",
+    BOLD: "Nunito_Sans_400Bold",
+  },
+
+  FONT_SIZES: {
+    XS: 12,
+    S: 14,
+    M: 16,
+    L: 18,
+    XL: 24,
+    XXL: 32,
+  },
+
+  LINE_HEIGHT: {
+    line_height: "130%",
+  },
+};
