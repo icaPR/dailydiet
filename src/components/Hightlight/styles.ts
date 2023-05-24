aa8a78146cf3ca4266e741c880c1ed4d0f255c11
Icaro Ramos <polosnramos@hotmail.com>
2023-05-24 03:23:10 -0300

    feat: component "Hightlight"

---
+++ src/components/Hightlight/styles.ts
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZES.XXL}px};
+    font-family: ${theme.FONT_WEIGHTS.BOLD};
+    color: ${theme.COLORS.GRAY_1};
+  `}
+`;
+
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZES.S}px;
+    font-family: ${theme.FONT_WEIGHTS.NORMAL};
+    color: ${theme.COLORS.GRAY_2};
+  `}
+`;
