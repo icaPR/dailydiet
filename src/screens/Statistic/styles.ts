2399bf3a319801e87f8f7b4daa7e6e57d88ff6fa
Icaro Ramos <polosnramos@hotmail.com>
2023-05-26 23:44:51 -0300

    feat: screen Statistic

---
+++ src/screens/Statistic/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.RED_LIGHT};
+`;
+
+export const GeneralStats = styled.View`
+  height: 80%;
+  margin-top: 24px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  padding: 24px;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_7};
+`;
+
+export const Title = styled.Text`
+  margin: 12px 0 12px 0;
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZES.M}px;
+    font-family: ${theme.FONT_WEIGHTS.BOLD};
+    color: ${theme.COLORS.GRAY_1};
+  `}
+`;
+
+export const MealTotalSequence = styled.View`
+  width: 100%;
+  height: 190px;
+  gap: 12px;
+  margin-bottom: 12px;
+`;
+
+export const MealSide = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+  justify-content: space-between;
+`;
