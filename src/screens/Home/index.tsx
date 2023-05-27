50292f79bdfa97b4060e8ca60bf5c27ab0e8b919
Icaro Ramos <polosnramos@hotmail.com>
2023-05-27 00:20:13 -0300

    feat: Navigator to screeen  "Statistic"

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,3 @@
-import { ButtonIcon } from "@components/ButtonIcon";
 import {
   Container,
   Percent,
@@ -6,15 +5,25 @@ import {
   Logo,
   Perfil,
   IconPoisiton,
+  Title,
 } from "./styles";
 import { Higtlight } from "@components/Hightlight";
 import { useState } from "react";
 import { ColorStyle } from "src/@types/styled";
+import { Button } from "@components/Button";
+import { Icon } from "@components/Icon";
+import { MealCard } from "@components/MealCard";
+import { useNavigation } from "@react-navigation/native";
 
 export function Home() {
   const [percentageStyle, setPercentageStyle] =
     useState<ColorStyle>("NEGATIVE");
-  const [percentageValue, setPercentageValue] = useState("20.00");
+  const [percentageValue, setPercentageValue] = useState("49,99");
+  const navigation = useNavigation();
+
+  function handleStatistic() {
+    navigation.navigate("statistic", { percentageValue, percentageStyle });
+  }
 
   return (
     <Container>
@@ -22,16 +31,18 @@ export function Home() {
         <Logo source={require("../../assets/Logo.png")} />
         <Perfil source={require("../../assets/Logo.png")} />
       </HeaderHome>
-
-      <Percent type={percentageStyle}>
+      <Percent type={percentageStyle} onPress={handleStatistic}>
         <IconPoisiton>
-          <ButtonIcon icon="arrow-top-right" type={percentageStyle} />
+          <Icon iconName="arrow-top-right" type={percentageStyle} />
         </IconPoisiton>
         <Higtlight
           title={percentageValue + "%"}
           subtitle="das refeições dentro da dieta"
         />
       </Percent>
+      <Title>Refeições</Title>
+      <Button title="Nova refeição" onPress={() => {}} />
+      <MealCard dateSnack="12.08.22" />
     </Container>
   );
 }
