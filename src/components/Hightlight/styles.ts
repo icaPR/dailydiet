561c046293fb496720f0f0f7d7a6d0e6929d9ebc
Icaro Ramos <polosnramos@hotmail.com>
2023-05-27 00:18:59 -0300

    style: Container

--- src/components/Hightlight/styles.ts
+++ src/components/Hightlight/styles.ts
@@ -1,8 +1,6 @@
 import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
