05edfa867283cde6709f9333affcb32d90574aeb
Icaro Ramos <polosnramos@hotmail.com>
2023-06-05 19:31:13 -0300

    refactor: type component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,17 @@
 import { TouchableOpacityProps } from "react-native/types";
 import { Container, Title } from "./styles";
 import { Icon } from "@components/Icon";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 type Props = TouchableOpacityProps & {
+  iconName?: keyof typeof MaterialCommunityIcons.glyphMap;
   title: string;
 };
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ iconName, title, ...rest }: Props) {
   return (
     <Container {...rest}>
-      <Icon iconName="plus" />
+      <Icon iconName={iconName} />
       <Title>{title}</Title>
     </Container>
   );
