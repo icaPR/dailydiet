75ec61ec35ed1f7ffae83f12816a0d68c5c0709c
Icaro Ramos <polosnramos@hotmail.com>
2023-11-24 05:18:49 -0300

    fix: small correction

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -12,6 +12,7 @@ export declare global {
         mealInSide?: string;
         mealOutSide?: string;
       };
+      newMeal: undefined;
     }
   }
 }
--- src/screens/Statistic/index.tsx
+++ src/screens/Statistic/index.tsx
@@ -27,7 +27,7 @@ export function Statistic() {
   }
 
   return (
-    <Container>
+    <Container type={percentageStyle}>
       <ButtonIcon
         icon="arrow-left"
         type={percentageStyle}
