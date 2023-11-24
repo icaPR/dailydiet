2c75ddd1f0dfc52e22f0f0da6ff515e03a6a559e
Icaro Ramos <polosnramos@hotmail.com>
2023-11-24 03:59:49 -0300

    feat: navigate newMeal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,22 +8,64 @@ import {
   Title,
 } from "./styles";
 import { Higtlight } from "@components/Hightlight";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ColorStyle } from "src/@types/styled";
 import { Button } from "@components/Button";
 import { Icon } from "@components/Icon";
 import { MealCard } from "@components/MealCard";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { getGroupsMeal } from "@storage/GroupMeal/getGroupsMeal";
+import { getMeal } from "@storage/Meal/getMeal";
+import { Alert, FlatList } from "react-native";
+import { MealStorageDTO } from "@storage/Meal/MealStorageDTO";
+
+type PropsDataMeal = {
+  time: string;
+  nameMeal: string;
+  status: boolean;
+};
 
 export function Home() {
-  const [percentageStyle, setPercentageStyle] =
-    useState<ColorStyle>("NEGATIVE");
-  const [percentageValue, setPercentageValue] = useState("49,99");
+  const [dataMeal, setDataMeal] = useState<MealStorageDTO[]>([]);
+  const [dataGroups, setDataGroups] = useState<string[]>([]);
+
+  const [percentageStyle, setPercentageStyle] = useState<ColorStyle>("NEUTRAL");
+  const [percentageValue, setPercentageValue] = useState("50");
+
   const navigation = useNavigation();
 
   function handleStatistic() {
+    if (parseFloat(percentageValue) > 50) {
+      setPercentageStyle("POSITIVE");
+    } else {
+      if (parseFloat(percentageValue) < 50) {
+        setPercentageStyle("NEGATIVE");
+      } else {
+        setPercentageStyle("NEUTRAL");
+      }
+    }
     navigation.navigate("statistic", { percentageValue, percentageStyle });
   }
+
+  function handleNewMeal() {
+    navigation.navigate("newMeal");
+  }
+
+  async function fetchGroups() {
+    try {
+      const dataGroups = await getGroupsMeal();
+      setDataGroups(dataGroups);
+      console.log("Aqui" + dataGroups);
+    } catch (error) {
+    } finally {
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
@@ -41,8 +83,12 @@ export function Home() {
         />
       </Percent>
       <Title>Refeições</Title>
-      <Button title="Nova refeição" onPress={() => {}} />
-      <MealCard dateSnack="12.08.22" />
+      <Button iconName="plus" title="Nova refeição" onPress={handleNewMeal} />
+      <FlatList
+        data={dataGroups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <MealCard dateSnack={item} />}
+      ></FlatList>
     </Container>
   );
 }
