f53d0585a133c1835ad7d574db5c15ddc3e54107
Icaro Ramos <polosnramos@hotmail.com>
2023-11-24 05:18:21 -0300

    feat: function show Meal

--- src/components/MealCard/index.tsx
+++ src/components/MealCard/index.tsx
@@ -1,15 +1,43 @@
 import { Meal } from "@components/Meal";
 import { Container, Title } from "./styles";
+import { useCallback, useEffect, useState } from "react";
+import { getMeal } from "@storage/Meal/getMeal";
+import { MealStorageDTO } from "@storage/Meal/MealStorageDTO";
+import { FlatList } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 type Props = {
   dateSnack: string;
 };
 
 export function MealCard({ dateSnack }: Props) {
+  const [dataMeal, setDataMeal] = useState<MealStorageDTO[]>([]);
+  async function fetchGroups(dateSnack: string) {
+    try {
+      const dataMeal = await getMeal(dateSnack);
+      setDataMeal(dataMeal);
+    } catch (error) {
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups(dateSnack);
+  }, [dateSnack]);
+
   return (
     <Container>
       <Title>{dateSnack}</Title>
-      <Meal time="20:00" title="X-Tudo" status={"NEGATIVE"} />
+      <FlatList
+        data={dataMeal}
+        keyExtractor={(item) => item.nameMeals}
+        renderItem={({ item }) => (
+          <Meal time={item.time} status={"NEUTRAL"} title={item.nameMeals} />
+        )}
+      ></FlatList>
     </Container>
   );
 }
+/*
+<Title>{dateSnack}</Title>
+<Meal time="20:00" title="X-Tudo" status={"NEGATIVE"} />*/
