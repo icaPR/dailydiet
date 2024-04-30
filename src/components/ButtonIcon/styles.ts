98e5cc393a97768791ca313f2e1991a773422feb
Icaro Ramos <polosnramos@hotmail.com>
2024-04-29 23:23:53 -0300

    feat: Update style in ButtonIcon component

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -2,10 +2,7 @@ import styled from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 
 export const Container = styled(TouchableOpacity)`
-  justify-content: flex-end;
-  margin: 36px 0 0 22px;
-  height: 28px;
-  width: 28px;
+  justify-content: center;
 `;
 
 //MaterialCommunityIcons
