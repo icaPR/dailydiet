489417a0707d4b7a2b67c784551ca4d0d95011bd
Icaro Ramos <polosnramos@hotmail.com>
2024-04-29 23:22:43 -0300

    feat: Update style in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,17 +2,26 @@ import { TouchableOpacityProps } from "react-native/types";
 import { Container, Title } from "./styles";
 import { Icon } from "@components/Icon";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { ButtonStyle, ColorStyle } from "src/@types/styled";
 
 type Props = TouchableOpacityProps & {
   iconName?: keyof typeof MaterialCommunityIcons.glyphMap;
   title: string;
+  type?: ButtonStyle;
+  colorIcon?: ColorStyle;
 };
 
-export function Button({ iconName, title, ...rest }: Props) {
+export function Button({
+  colorIcon,
+  type = "DARK",
+  iconName,
+  title,
+  ...rest
+}: Props) {
   return (
-    <Container {...rest}>
-      <Icon iconName={iconName} />
-      <Title>{title}</Title>
+    <Container type={type} {...rest}>
+      <Icon iconName={iconName} type={colorIcon} />
+      <Title type={type}>{title}</Title>
     </Container>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,21 +1,39 @@
-import { TouchableOpacity } from "react-native";
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  TextProps,
+} from "react-native";
 import styled, { css } from "styled-components/native";
+import { ButtonStyle } from "src/@types/styled";
 
-export const Container = styled(TouchableOpacity)`
+type PropsButton = TouchableOpacityProps & {
+  type: ButtonStyle;
+};
+type PropsButtonText = TextProps & {
+  type: ButtonStyle;
+};
+
+export const Container = styled(TouchableOpacity)<PropsButton>`
   width: 100%;
-  height: 50;
+  height: 50px;
+  margin: 5px 0;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   border-radius: 6px;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_2};
-`;
-
-export const Title = styled.Text`
-  margin-left: 12;
-  ${({ theme }) => css`
-    font-size: ${theme.FONT_SIZES.M}px;
-    font-weight: ${theme.FONT_WEIGHTS.BOLD};
-    color: ${theme.COLORS.WHITE};
+  ${({ theme, type }) => css`
+    background-color: ${type === "WHITE"
+      ? theme.COLORS.GRAY_7
+      : theme.COLORS.GRAY_1};
+    border-width: ${type === "WHITE" ? 2 : 0};
+    border-color: ${type === "WHITE" ? theme.COLORS.GRAY_1 : 0};
   `}
+`;
+export const Title = styled.Text<PropsButtonText>`
+  margin: 12px
+    ${({ theme, type }) => css`
+      font-size: ${theme.FONT_SIZES.M}px;
+      font-family: ${theme.FONT_WEIGHTS.BOLD};
+      color: ${type === "WHITE" ? theme.COLORS.GRAY_1 : theme.COLORS.WHITE};
+    `};
 `;
