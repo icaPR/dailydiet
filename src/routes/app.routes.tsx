25d4bcbcc5d2b531d366d620de8e79c8cbfdebcf
Icaro Ramos <polosnramos@hotmail.com>
2024-05-01 23:17:01 -0300

    feat: Add cardMeal and editCard screens to routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,8 @@ import { Home } from "@screens/Home";
 import { Statistic } from "@screens/Statistic";
 import { NewMeal } from "@screens/NewMeal";
 import { MessageScreen } from "@screens/Message";
+import { CardMeal } from "@screens/CardMeal";
+import { EditCard } from "@screens/Edit";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ export function AppRoutes() {
       <Screen name="statistic" component={Statistic} />
       <Screen name="newMeal" component={NewMeal} />
       <Screen name="messageScreen" component={MessageScreen} />
+      <Screen name="cardMeal" component={CardMeal} />
+      <Screen name="editCard" component={EditCard} />
     </Navigator>
   );
 }
